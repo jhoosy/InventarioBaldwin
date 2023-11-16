44873bccd1bfbd76d55f3fb0a28f0bf7f2217aae
Jhoss <maycolmedina367@gmail.com>
2023-11-16 12:02:33 -0500

    cree un nuevo Login , para mejorara un poco la estetica

--- vistas/login.php
+++ vistas/login.php
@@ -3,41 +3,33 @@
 
 <head>
     <title>Animated Login Form</title>
-    <link rel="stylesheet" type="text/css" href="css/style.css">
+    <link rel="stylesheet" type="text/css" href="css/login.css">
     <link href="https://fonts.googleapis.com/css?family=Poppins:600&display=swap" rel="stylesheet">
     <script src="https://kit.fontawesome.com/a81368914c.js"></script>
 </head>
 
 <body>
-    <img class="wave" src="img/wave.png">
-    <div class="container">
-        <div class="img">
-            <img src="IMG/LOGO-ESCUDO-BALDWIN-PNG-1.png">
-        </div>
-        <div class="login-content">
-            <form class="box login" action="" method="POST" autocomplete="off">
-                <img src="img/txtlog.png"><br><br>
-                <h2>INVENTARIO</h2>
-                <div class="input-div one">
-                    <div class="i">
-                        <i class="fas fa-user"></i>
-                    </div>
-                    <div class="div">
-                        <h5>Usuario</h5>
-                        <input class="input" type="text" name="login_usuario" pattern="[a-zA-Z0-9]{4,20}" maxlength="20" required>
-                    </div>
-                </div>
-                <div class="input-div pass">
-                    <div class="i">
-                        <i class="fas fa-lock"></i>
-                    </div>
-                    <div class="div">
-                        <h5>Contraseña</h5>
-                        <input class="input" type="password" name="login_clave" pattern="[a-zA-Z0-9$@.-]{3,100}" maxlength="100" required>
-                    </div>
-                </div>
+        <div class="body"></div>
+		<div class="grad"></div>
+		<div class="header">
+          <div class="logo">
+         <img src="img/logg2.png" alt="Logo de Baldwin">
+          </div>
+			
+		</div>
+		<br>
+        
+        <div class="login">
+           <form  action="" method="POST" autocomplete="off">
+                <div>INVENTARIO</div><br>
+				
+                <input class="input" type="text" placeholder="Usuario" name="login_usuario" pattern="[a-zA-Z0-9]{4,20}" maxlength="20" required><br>
+                 <!-- #region -->
+				
+                <input class="input" type="password" placeholder="Contraseña" name="login_clave" pattern="[a-zA-Z0-9$@.-]{3,100}" maxlength="100" required><br>
 
-                <input type="submit" class="btn" value="INGRESAR">
+			
+                <input type="submit" class="button" value="INGRESAR">  
 
                 <?php
                 if(isset($_POST['login_usuario']) && isset($_POST['login_clave'])){
@@ -47,9 +39,15 @@
                 ?>
 
             </form>
-        </div>
-    </div>
-    <script type="text/javascript" src="js/login.js"></script>
+
+
+         </div>
 </body>
 
 </html>
+ 
+
+
+
+
+ 
